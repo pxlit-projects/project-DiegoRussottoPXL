dd48782a42f859a92058229a485c47357269944f
Diego Russotto <12000904@student.pxl.be>
2024-11-28 23:08:59 +0100

    Added logback

--- backend-java/Contentbeheer_backend/post-service/src/main/java/be/pxl/services/controller/PostController.java
+++ backend-java/Contentbeheer_backend/post-service/src/main/java/be/pxl/services/controller/PostController.java
@@ -3,6 +3,8 @@ package be.pxl.services.controller;
 import be.pxl.services.domain.dto.PostRequest;
 import be.pxl.services.service.IPostService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class PostController {
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
+
     private final IPostService postService;
 
     @GetMapping
     public ResponseEntity getAllPosts(){
+        log.info("Fetching all posts");
         return new ResponseEntity(postService.getAllPosts(), HttpStatus.OK);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void addPost(@RequestBody PostRequest postRequest){
+        log.info("Creating new post with title: {}", postRequest.getTitle());
         postService.addPost(postRequest);
     }
 }
