86d42c8a07013309c328eb09b5d46b562ef2edd0
Diego Russotto <12000904@student.pxl.be>
2024-12-03 16:05:48 +0100

    routing frontend

--- frontend-web/Contentbeheer_backend/src/app/app.component.ts
+++ frontend-web/Contentbeheer_backend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CreatePostComponent } from "./components/posts/create-post/create-post.component";
 import { PostListComponent } from "./components/posts/post-list/post-list.component";
 import { HomepageComponent } from "./components/posts/homepage/homepage.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CreatePostComponent, PostListComponent, HomepageComponent],
+  imports: [RouterOutlet, CreatePostComponent, PostListComponent, HomepageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend-web/Contentbeheer_backend/src/app/app.routes.ts
+++ frontend-web/Contentbeheer_backend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CreatePostComponent } from './components/posts/create-post/create-post.component';
+import { HomepageComponent } from './components/posts/homepage/homepage.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    {path: 'home', component : HomepageComponent},
+    {path: 'add', component : CreatePostComponent},
+    {path: 'posts', component : PostListComponent},
+    { path: '**', component: HomepageComponent }
+];
